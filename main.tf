45ca7a4e38ccdde8ce94c858c514302e85ac2e7e
Michael.G@minutemenmco.com <Michael.G@minutemenmco.com>
2026-04-23 12:44:16 -0400

    Use static Ubuntu AMI for AWS Academy IAM restrictions

--- main.tf
+++ main.tf
@@ -5,20 +5,13 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = var.instance_type
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  tags = {
+    Name = var.instance_name
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "ubuntu" {
